8f78580aaf6143456453887c9b19b868f3fb91ba
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2025-08-13 17:55:11 -0600

    Created my first async blueprint node
    Created my first async blueprint node
    
    with additional execution pins

---
+++ Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -0,0 +1,29 @@
+// Sebastian Lara. All rights reserved
+
+
+#include "AsyncActions/AsyncAction_PushSoftWidget.h"
+
+UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject,
+                                                                         APlayerController* OwningPlayerController,
+                                                                         TSoftClassPtr<UWidget_ActivatableBase>
+                                                                         InSoftWidgetClass,
+                                                                         UPARAM(
+	                                                                         meta = (Categories = "Frontend.WidgetStack"
+	                                                                         ))
+                                                                         FGameplayTag InWidgetStackTag,
+                                                                         bool bFocusOnNewlyPushedWidget)
+{
+	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class"));
+
+	if (GEngine)
+	{
+		if (UWorld* const World = GEngine->GetWorldFromContextObject(WorldContextObject,
+		                                                             EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+			Node->RegisterWithGameInstance(World);
+			return Node;
+		}
+	}
+	return nullptr;
+}
---
+++ Source/FrontendUI/Public/AsyncActions/AsyncAction_PushSoftWidget.h
@@ -0,0 +1,38 @@
+// Sebastian Lara. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GameplayTagContainer.h"
+#include "AsyncAction_PushSoftWidget.generated.h"
+
+class UWidget_ActivatableBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPushSoftWidget, UWidget_ActivatableBase*, PushedWidget);
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDUI_API UAsyncAction_PushSoftWidget : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable,
+		meta = (WorldContext = "WorldContextObject", HidePin = "WorldContextObject", BlueprintInternalUseOnly = "true",
+			DisplayName = "Push Soft Widget To Widget Stack"))
+	static UAsyncAction_PushSoftWidget* PushSoftWidget(const UObject* WorldContextObject,
+	                                                   APlayerController* OwningPlayerController,
+	                                                   TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
+	                                                   UPARAM(meta = (Categories = "Frontend.WidgetStack"))
+	                                                   FGameplayTag InWidgetStackTag,
+	                                                   bool bFocusOnNewlyPushedWidget = true);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidget OnWidgetCreatedBeforePush;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPushSoftWidget AfterPush;
+};
